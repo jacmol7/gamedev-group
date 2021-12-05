0538e186de0dbaabd5e2a807e126a755c57c8f3c
jacmol7 <jacob.amazing1@gmail.com>
2021-12-05 20:45:58 +0000

    Add a rate limit to the player shooting speed

--- Assets/Scripts/PlayerShootingController.cs
+++ Assets/Scripts/PlayerShootingController.cs
@@ -6,9 +6,11 @@ public class PlayerShootingController : MonoBehaviour
 {
     public GameObject projectile;
     public float projectileSpeed;
+    public float fireRate;
     
     private Vector2 aimDirection;
     private GameObject bulletPool;
+    private bool canShoot;
 
     void Start()
     {
@@ -17,20 +19,35 @@ public class PlayerShootingController : MonoBehaviour
         {
             bulletPool = new GameObject("PlayerBullets");
         }
+
+        canShoot = true;
     }
 
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        aimDirection = (mousePos - transform.position).normalized;
-        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
-
         if (Input.GetMouseButtonDown(0))
         {
+            StartCoroutine("shoot");
+        }
+    }
+
+    private IEnumerator shoot()
+    {
+        if (canShoot)
+        {
+            canShoot = false;
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            aimDirection = (mousePos - transform.position).normalized;
+            float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+            
             GameObject p = Instantiate(projectile, transform.position, transform.rotation, bulletPool.transform);
             p.transform.eulerAngles = new Vector3(0, 0, angle);
             p.GetComponent<Rigidbody2D>().velocity = p.transform.right * projectileSpeed;
+            
+            yield return new WaitForSeconds(fireRate);
+            canShoot = true;
         }
     }
 }
