727a05fe1162467050b363d858be2dfc359d40be
huichaksum <csh32@kent.ac.uk>
2021-12-08 13:21:29 +0000

    Change bullet name to missile

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Missile : MonoBehaviour
 {
     public float speed; 
 
@@ -13,7 +13,6 @@ public class Bullet : MonoBehaviour
     public GameObject explode;
     float removeTime = 2.0f; 
 
-    //public GameObject player;
 
     // Start is called before the first frame update
     void Start()
