c1667aec846b34ae6451f770a63b02313a9d0c95
jacmol7 <jacob.amazing1@gmail.com>
2021-12-18 16:59:43 +0000

    Allow the minigame cursor to start anywhere independant from the actual mouse cursor

--- Assets/Scripts/MiniGameCursor.cs
+++ Assets/Scripts/MiniGameCursor.cs
@@ -6,6 +6,11 @@ public class MiniGameCursor : MonoBehaviour
 {
     MiniGameManager miniGameManager;
 
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Start()
     {
         miniGameManager = GameObject.Find("MiniGameManager").GetComponent<MiniGameManager>();
@@ -13,13 +18,18 @@ public class MiniGameCursor : MonoBehaviour
 
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-        transform.position = new Vector3(mousePos.x, mousePos.y, 1);
+        // Move the cursor based off mouse movement rather than using the position.
+        // This allows the game cursor to start anywhere regardless of where the mouse is
+        transform.position += new Vector3(Input.GetAxis("Mouse X") * 0.1f, Input.GetAxis("Mouse Y") * 0.1f, 0);
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         miniGameManager.endMiniGame(col.gameObject.tag == "Goal");
+    }
+
+    void OnDestroy()
+    {
+        Cursor.lockState = CursorLockMode.None;
     }
 }
