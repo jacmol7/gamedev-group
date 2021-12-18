8026a91fbe34b8d88d0cc258c8e16b0d9b5f0f73
huichaksum <csh32@kent.ac.uk>
2021-12-18 18:54:39 +0000

    adding comments

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
-    public float health;
-    public GameObject enemy;
+    public float health; //health of enemy 
+    
     // Start is called before the first frame update
     void Start()
     {
-        enemy = GameObject.Find("enemy1");
+        
     }
 
-    public void reduceHealth()
+    public void reduceHealth() //method to reduce enemy health 
     {
-        health--;
-        Debug.Log("Reduce health");
+        health--; //minus one health for each hit 
+        //Debug.Log("Reduce health");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //health--;
-        if(health < 1)
+        if(health < 1) //if enemy health is lower than 1
         {
-            Destroy(gameObject);
-            //enemy.GetComponent<enemy1>().OnDestroy();
-    
+            Destroy(gameObject); //destroy the enemy 
         }
     }
 
