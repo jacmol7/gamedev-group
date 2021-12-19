f2fc0a6220395662c7c8c0ac72e8e95d58cd7fc8
huichaksum <csh32@kent.ac.uk>
2021-12-19 12:49:56 +0000

    adding comments

--- Assets/Scripts/EnemyPatrolMove.cs
+++ Assets/Scripts/EnemyPatrolMove.cs
@@ -17,18 +17,18 @@ public class EnemyPatrolMove : MonoBehaviour
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime); //set the direction to the right, making the speed as a real time 
 
-        RaycastHit2D ray = Physics2D.Raycast(groundDetect.position, Vector2.down, distance, LayerMask.GetMask("Ground")); //vector2 check whther if the enemy is dectected the ground 
+        RaycastHit2D ray = Physics2D.Raycast(groundDetect.position, Vector2.down, distance, LayerMask.GetMask("Ground")); //vector2 check whther if the enemy is dectected the ground, layer mask for the enemy to detect the ground mask layer 
         if (ray.collider == false) //statement if raycast isn;t collide with anything  
         {
-            if (movingRight == false)//if enemy
+            if (movingRight == false)//if enemy the enemy is moving to the left 
             {
-                transform.eulerAngles = new Vector3(0, 0, 0);
-                movingRight = true;
+                transform.eulerAngles = new Vector3(0, 0, 0); //reset the y value to 0 
+                movingRight = true; //enemy move to right if it hits the edge or wall 
             }
             else //if the enenmy is moving to the right 
             {
-                transform.eulerAngles = new Vector3(0, -180, 0);
-                movingRight = false;
+                transform.eulerAngles = new Vector3(0, -180, 0); //rotate the enemy 180 degree on y-axis 
+                movingRight = false; //enemy move to left if it hits the edge or wall 
                 
             }
         }
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,16 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GamePaused = false;
+    public static bool GamePaused = false; //setting the game pause to false 
 
-    public GameObject pauseMenu;
+    public GameObject pauseMenu; //pause menu game object 
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.Escape)) //apply functon when click on esc 
         {
-            if(GamePaused)
+            if(GamePaused) //when game is paused 
             {
                 Resume();
             }
@@ -25,23 +25,22 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    public void Resume()
+    public void Resume() //resume the game 
     {
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1.0f;
-        GamePaused = false;
+        pauseMenu.SetActive(false); //hide the pause menu when game is resumed 
+        Time.timeScale = 1.0f; //set the game to normal time scale 
+        GamePaused = false; //make the game knows it's not pausing 
     }
 
-    void Pause()
+    void Pause() //pause the game 
     {
-        pauseMenu.SetActive(true);
-        Time.timeScale = 0.0f;
-        GamePaused = true;
+        pauseMenu.SetActive(true); //show the pasue menu when game is paused 
+        Time.timeScale = 0.0f; //stop the game from running 
+        GamePaused = true; //make the game knows it's pausing 
     }
 
-    public void LoadMenu()
+    public void LoadMenu() //button function for main menu 
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-        Debug.Log("Back to menu");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1); //load the main menu
     }
 }
