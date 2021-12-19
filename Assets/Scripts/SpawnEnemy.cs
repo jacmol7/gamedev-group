657c8b9dd37ffecab78e1d68cffe3e7565595e27
huichaksum <csh32@kent.ac.uk>
2021-12-19 12:12:45 +0000

    adding comments

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -4,46 +4,6 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
-    [SerializeField]
-    private float speed; 
-
-    [SerializeField]
-    private Vector3[] position; 
-
-    private int index;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    void EnemyMovement()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = Vector2.MoveTowards(transform.position, position[index], Time.deltaTime * speed);
-
-        if (transform.position == position[index])
-        {
-            if(index == position.Length -1)
-            {
-                index = 0;
-                Debug.Log("movement2");
-            }
-            else 
-            {
-                index++;
-                Debug.Log("movement3");
-            }
-        }
-        Debug.Log("movement");
-    }
-
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
